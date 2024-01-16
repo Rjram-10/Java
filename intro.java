6917d197a611159731d6168778b6d00cbea91cd1
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-01-16 23:34:36 +0530

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,43 @@
+/*Java feature :- 
+
+1.Simple =  easy to learn , removed rarely used features like explicit pointers , operator overloading,
+            no need to remove unreferenced objects because there is an Automatic Garbage Collection in 
+            Java.
+
+2.Robust =  strong memory mangement means efficiently allocated and deallocated memory space , 
+            avoid memory leaks , minimize the risk of error and crashes.It have Exception handling ,
+            type checking features.
+
+3.Platform independent = It's JVM converts the code into bytecode that is platform independent and 
+                         can be run on any os and any other language is also converted into bytecode 
+                         would be platform independent.Java provides software-based platform.
+                        WORA (Write Once and Run Anywhere).
+
+4.Portable = can carry to any platform in form of bytecode
+
+5.Secure = have no pointer , uses runtime environment of it's own , not of its platform like C,C++
+           Classloader = provide security by separting the packages of classes of local file from 
+                         those imported from internet.
+            
+
+6.OOPs = Object-oriented means we organize our software as a combination of different types of objects 
+        that incorporate both data(attribute) and behavior(method or function).Contains = class ,objects,
+        encapsulation , polymorphism , inheritence and abstraction
+
+7.Archietectural neutral = There is no implementation dependent feature eg. int size is not based *32 bit
+                           system and 64 bit system
+
+8.High performance = have faster than any interpreted language because bytecode is close to native
+                     language(C,C++) but slower that a compiler language.
+
+9.Dynamic =  It support dynamic loading(load on demand) of classes.
+
+10.Interpreted = 
+11.Distributed = with this we can access files by calling the method from any machine on internet.
+
+12.Multithreaded = Java can write program that runs multiple task at a time by defining threads.
+                    Here multiple thread share common memory area.
+*/
+public class intro {
+
+}
