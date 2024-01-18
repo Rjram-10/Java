a30bbf04adb0b05d23d464ee867ac4261cb4cd04
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-01-19 01:45:52 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter No.of Subjects :- ");
+        int n=sc.nextInt();
+        float total=0.0f;
+        for(int i=0;i<n;i++){
         System.out.print("Enter Subject marks:- ");
         int a=sc.nextInt();
-        System.out.print("Enter Subject marks:- ");
-        int b=sc.nextInt();
-        System.out.print("Enter Subject marks:- ");
-        int c=sc.nextInt();
-        System.out.print("Enter Subject marks:- ");
-        int d=sc.nextInt();
-        System.out.print("Enter Subject marks:- ");
-        int e=sc.nextInt();
-        // float sum=a+b+c+d+e;
-        System.out.println("Percentage of Student is :- "+(((float)(a+b+c+d+e)/500)*100));
-
+        total+=a;
+        }
+        System.out.println("Percentage of Student is :- "+(total/n));
+        sc.close()
     }
 }
