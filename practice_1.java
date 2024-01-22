c26e6456b6930c4448f4d78bfc2fca491a562e49
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-01-23 00:06:09 +0530

    Update practice_1.java

--- practice_1.java
+++ practice_1.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 public class practice_1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        
         System.out.print("Enter the number of participants :- ");
         int num=sc.nextInt();
+        
         String arr[]=new String[15];
+        
         for (int i=0 ; i<num;i+=2) {
+            
             System.out.print("Enter name :- ");
             arr[i]=sc.next();
+            
             System.out.print("Enter age :- ");
             arr[i+1]=sc.next();
         }
+        
         for(int i=0;i<num;i+=2){
+            
             int j=Integer.parseInt(arr[i+1]);
+            
             if(j>=18){
                 System.out.println(arr[i]+" is eligible for vote");
             }
