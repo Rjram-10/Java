998df350e72d43424ce6a189b570bc709fb2a90a
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-01-22 23:57:13 +0530

    Add files via upload

---
+++ practice_1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class practice_1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String arr[]=new String[15];
+        for (int i=0 ; i<4;i+=2) {
+            System.out.print("Enter name :- ");
+            arr[i]=sc.next();
+            System.out.print("Enter age :- ");
+            arr[i+1]=sc.next();
+        }
+        for(int i=0;i<4;i+=2){
+            int j=Integer.parseInt(arr[i+1]);
+            if(j>=18){
+                System.out.println(arr[i]+" is eligible for vote");
+            }
+        }
+  
+}
+}
