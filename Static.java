7bdd14f330903e5650806770c06e1fae0dae0606
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-02-02 03:07:07 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,29 @@
+/* static = a single copy of method/variable is created and shared among the objects the class
+            throughout all obejcts the variable/method have same value and it could accessed without
+            using object
+   */
+class Friend{
+    String name;
+    static int numoffriend;
+
+    Friend(String name){
+        this.name=name;
+        numoffriend++;
+    }
+
+    static void Display(){
+        System.out.println("The number of friends is "+numoffriend);
+    }
+}
+
+public class Static {
+    public static void main(String[] args) {
+        
+        Friend friend1=new Friend("Batman");
+        Friend friend2=new Friend("Superman");
+        Friend friend3=new Friend("Flash");
+
+        Friend.Display(); // It counts the no. of friends object made through class Friend through 
+                            // static variable numoffriend and static method Display()
+    }
+}
