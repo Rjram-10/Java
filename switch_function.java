1dd9debde1b0fa253bef7ddd31b451919d02b6d2
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-02-02 02:47:28 +0530

    Update switch_function.java

--- switch_function.java
+++ switch_function.java
@@ -7,8 +7,8 @@ public static void main(String[] args) {
             case "Shubham" :
                 System.out.println("You are selected in company");
                 break;
-            //case "Aman" -> System.out.println("Being patience is key to success"); it's a way of
-            //  writing switch case in intense switch mode supported only above Java 14
+            //case "Aman" -> System.out.println("Being patience is key to success"); 
+                       // it's a way of writing switch case in intense switch mode supported only above Java 14
             case "Param":
                 System.out.println("Part of team");
             default:
