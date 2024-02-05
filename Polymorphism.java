a32ca2d2cef4a5fc08066d9245831e3a8dbc6b23
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-02-05 23:30:14 +0530

    Add files via upload

---
+++ Polymorphism.java
@@ -0,0 +1,39 @@
+/*Polymorphism = it means many forms  */
+
+class Vechile{
+    public void go(){
+
+    }
+}
+class Boat extends Vechile{
+    public void go(){
+        System.out.println("The Boat is in race");
+    }
+}
+class Bicycle extends Vechile{
+    public void go(){
+        System.out.println("The Bicycle is in race");
+    }
+}
+
+class Car extends Vechile{
+    public void go(){
+        System.out.println("The Car is in race");
+    }
+}
+
+public class Polymorphism {
+
+    public static void main(String[] args) {
+        Car car=new Car();
+        Bicycle bicycle = new Bicycle();
+        Boat boat=new Boat();
+
+        Vechile[] racers={car,bicycle,boat};
+
+        for (Vechile x : racers) {
+            x.go();
+        }
+    }
+    
+}
