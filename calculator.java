361b21083c19371527f47cbce29e42a0a692b486
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-02-11 23:56:25 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -6,7 +6,7 @@ public class calculator {
         System.out.print("Enter No.of Subject :- ");
         int n=sc.nextInt();
         float total=0.0f;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ 
         System.out.print("Enter Subject marks:- ");
         int a=sc.nextInt();
         total+=a;
