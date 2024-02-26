7e6e25ef9a85db6b92f993d7ca333739566cf0ea
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-02-26 23:57:31 +0530

    Update Variable_argument.java

--- Variable_argument.java
+++ Variable_argument.java
@@ -4,7 +4,7 @@ public class Variable_argument {
     // public static int sum(int ...arr) we can write in this format also and it indicates that it 
                                     //  doesn't need an argument to run the  methods , it would run without it also               
     public static int sum(int x,int ...arr){
-
+ 
         int result =x;
         
         for(int i:arr){
