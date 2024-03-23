c9bbedbdc5aca2d501115c13a1269d37d0e6934c
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-03-23 23:50:26 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -9,7 +9,7 @@ public class calculator {
         for(int i=0;i<n;i++){ 
         System.out.print("Enter Subject marks:- ");
         int a=sc.nextInt();
-        total+=a;
+        total+=a; 
         }
         System.out.println("Percentage of Student is :- "+(total/n));
         sc.close()
